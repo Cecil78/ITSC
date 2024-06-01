b39697af5a3012df78f8bbefe9df3158d5c5e5c1
Cecil <123985852+Cecil78@users.noreply.github.com>
2024-06-01 10:40:17 -0400

    Add files via upload

---
+++ CrystalReportsApplication1/CrystalReportsApplication1/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CrystalReportsApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CrystalReportsApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3d1e051-b996-4528-a071-17ee93d24c8d")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EJconversion29/EJconversion29/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EJconversion29
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double Metros = 1609.344; //Una milla = 1609.344 metros
+            double Kilometros = 1.60934; //Una milla = 1.60934 kilómetros
+            double resultado;
+            int numeroMilla;
+            Console.WriteLine("Ingrese las millas que desea convertir \n");
+
+            numeroMilla = Convert.ToInt32(Console.ReadLine());
+            resultado = numeroMilla * Metros;
+
+            Console.WriteLine("{0} millas es igual a {1} metros",
+            numeroMilla, resultado);
+
+            resultado = numeroMilla * Kilometros;
+
+            Console.WriteLine("{0} millas es igual a {1} kilometros", numeroMilla, resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
+
